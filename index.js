43488a25025f99769d1e85181bc20173b5b4a96c
Fabio Giannetti <fgiannet@cisco.com>
2016-05-23 17:06:26 -0700

    Nuovo Messaggio

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Benvenuti a Cisco Shipped!');
+    res.send('Benvenuti finalmente a Cisco Shipped!');
 });
 
 var server = app.listen(3000, function () {
